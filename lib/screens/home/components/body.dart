0b0ca70b2dcb75bcba5e30f480658154335d94ac
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-23 02:01:33 +0700

    ADD - camera fitur on logo

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,11 +1,102 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:plant_app/screens/home/bloc/camera_bloc.dart';
+import 'package:plant_app/screens/home/bloc/camera_page.dart';
 import 'package:plant_app/screens/home/components/featurred_plants.dart';
 import 'package:plant_app/screens/home/components/header_with_searchbox.dart';
 import 'package:plant_app/screens/home/components/recommend_plants.dart';
 import 'package:plant_app/screens/home/components/title_with_more_bbtn.dart';
 import 'package:plant_app/theme_constants.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
+  const Body({super.key});
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  String selectedAddress = '';
+  File? _profileImage;
+
+  Future<void> _openCamera() async {
+    final file = await Navigator.push<File>(
+      context,
+      MaterialPageRoute(
+        builder:
+            (_) => BlocProvider(
+              create: (_) => CameraBloc()..add(InitializeCamera()),
+              child: CameraPage(),
+            ),
+      ),
+    );
+
+    if (file != null && mounted) {
+      setState(() {
+        _profileImage = file;
+      });
+    }
+  }
+
+  void _showImageOptions() {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (_profileImage != null)
+                ListTile(
+                  leading: Icon(Icons.delete, color: Colors.red),
+                  title: Text('Delete Image'),
+                  onTap: () {
+                    setState(() {
+                      _profileImage = null;
+                    });
+                    Navigator.pop(context);
+                  },
+                ),
+              ListTile(
+                leading: Icon(Icons.photo_camera),
+                title: Text('Take Photo'),
+                onTap: () {
+                  Navigator.pop(context);
+                  _openCamera(); // panggil fungsi kamera
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.photo_library),
+                title: Text('Import from Gallery'),
+                onTap: () async {
+                  final pickedFile = await ImagePicker().pickImage(
+                    source: ImageSource.gallery,
+                  );
+                  if (pickedFile != null) {
+                    setState(() {
+                      _profileImage = File(pickedFile.path);
+                    });
+                  }
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.cancel),
+                title: Text('Cancel'),
+                onTap: () => Navigator.pop(context),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // give total height and width of our screen
@@ -15,12 +106,16 @@ class Body extends StatelessWidget {
       child: Column(
         // crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          HeaderWithSearchBox(size: size),
+          HeaderWithSearchBox(
+            size: size,
+            onCameraTap: _showImageOptions,
+            profileImage: _profileImage,
+          ),
           TitleWithMoreBbtn(title: "Recomended", press: () {}),
           RecommendPlants(),
-          TitleWithMoreBbtn(title: "Featured Plants", press: (){}),
+          TitleWithMoreBbtn(title: "Featured Plants", press: () {}),
           FeaturePlants(),
-          SizedBox(height: AppSpacing.defaultPadding,)
+          SizedBox(height: AppSpacing.defaultPadding),
         ],
       ),
     );
--- lib/screens/home/components/header_with_searchbox.dart
+++ lib/screens/home/components/header_with_searchbox.dart
@@ -1,26 +1,39 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app/screens/home/bloc/camera_bloc.dart';
 import 'package:plant_app/theme_constants.dart';
 
 class HeaderWithSearchBox extends StatelessWidget {
-  const HeaderWithSearchBox({super.key, required this.size});
+  const HeaderWithSearchBox({
+    super.key,
+    required this.size,
+    required this.onCameraTap,
+    this.profileImage,
+  });
 
   final Size size;
+  final VoidCallback onCameraTap;
+  final File? profileImage;
 
   @override
   Widget build(BuildContext context) {
-    // Size size = MediaQuery.of(context).size;
     return Container(
       margin: EdgeInsets.only(bottom: AppSpacing.defaultPadding * 2.5),
       height: size.height * 0.2,
       child: Stack(
         children: <Widget>[
           Container(
-            padding: EdgeInsets.only(left: AppSpacing.defaultPadding, right: AppSpacing.defaultPadding, bottom: 36 + AppSpacing.defaultPadding),
+            padding: EdgeInsets.only(
+              left: AppSpacing.defaultPadding,
+              right: AppSpacing.defaultPadding,
+              bottom: 36 + AppSpacing.defaultPadding,
+            ),
             height: size.height * 0.2 - 27,
             decoration: BoxDecoration(
               color: AppColors.primary,
-              borderRadius: BorderRadius.only(
+              borderRadius: const BorderRadius.only(
                 bottomLeft: Radius.circular(36),
                 bottomRight: Radius.circular(36),
               ),
@@ -34,15 +47,21 @@ class HeaderWithSearchBox extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                Spacer(),
-                ClipOval(
-                  child: Container(
-                    width: 60,
-                    height: 60,
-                    color: Colors.white,
-                    child: Image.asset(
-                      'assets/images/logo.png',
-                      fit: BoxFit.cover,
+                const Spacer(),
+                GestureDetector(
+                  onTap: () => onCameraTap(),
+                  child: ClipOval(
+                    child: Container(
+                      width: 60,
+                      height: 60,
+                      color: Colors.white,
+                      child:
+                          profileImage != null
+                              ? Image.file(profileImage!, fit: BoxFit.cover)
+                              : Image.asset(
+                                'assets/images/logo.png',
+                                fit: BoxFit.cover,
+                              ),
                     ),
                   ),
                 ),
@@ -55,7 +74,9 @@ class HeaderWithSearchBox extends StatelessWidget {
             right: 0,
             child: Container(
               alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: AppSpacing.defaultPadding),
+              margin: EdgeInsets.symmetric(
+                horizontal: AppSpacing.defaultPadding,
+              ),
               padding: EdgeInsets.symmetric(
                 horizontal: AppSpacing.defaultPadding,
               ),
@@ -65,9 +86,9 @@ class HeaderWithSearchBox extends StatelessWidget {
                 borderRadius: BorderRadius.circular(20),
                 boxShadow: [
                   BoxShadow(
-                    offset: Offset(0, 10),
+                    offset: const Offset(0, 10),
                     blurRadius: 50,
-                    color: AppColors.primary.withValues(alpha: 0.23),
+                    color: AppColors.primary.withOpacity(0.23),
                   ),
                 ],
               ),
@@ -79,7 +100,7 @@ class HeaderWithSearchBox extends StatelessWidget {
                       decoration: InputDecoration(
                         hintText: "Search",
                         hintStyle: TextStyle(
-                          color: AppColors.primary.withValues(alpha: 0.5),
+                          color: AppColors.primary.withOpacity(0.5),
                         ),
                         enabledBorder: InputBorder.none,
                         focusedBorder: InputBorder.none,
