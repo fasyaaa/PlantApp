c060e08264597a92c7afd928d28a7f67646d197f
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-22 18:27:59 +0700

    ADD - RecommendPlantCard list with navigation to DetailScreen

---
+++ lib/screens/home/components/recommend_plants.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/theme_constants.dart';
+
+class RecommendPlants extends StatelessWidget {
+  const RecommendPlants({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          RecommendPlantCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailScreen()),
+              );
+            },
+          ),
+          RecommendPlantCard(
+            image: "assets/images/image_2.png",
+            title: "Angelica",
+            country: "Russia",
+            price: 440,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailScreen()),
+              );
+            },
+          ),
+          RecommendPlantCard(
+            image: "assets/images/image_3.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
+
+class RecommendPlantCard extends StatelessWidget {
+  const RecommendPlantCard({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.country,
+    required this.price,
+    required this.press,
+  });
+
+  final String image, title, country;
+  final int price;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.only(
+        left: AppSpacing.defaultPadding,
+        right: AppSpacing.defaultPadding,
+        top: AppSpacing.defaultPadding / 2,
+        bottom: AppSpacing.defaultPadding * 2.5,
+      ),
+      width: size.width * 0.4,
+      child: Column(
+        children: <Widget>[
+          Image.asset(image),
+          GestureDetector(
+            onTap: press,
+            child: Container(
+              padding: EdgeInsets.all(AppSpacing.defaultPadding / 2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: AppColors.primary.withValues(alpha: 0.23),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "${title.toUpperCase()}\n",
+                          style: Theme.of(context).textTheme.labelLarge,
+                        ),
+                        TextSpan(
+                          text: "${title.toUpperCase()}",
+                          style: TextStyle(
+                            color: AppColors.primary.withValues(alpha: 0.5),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    '\$$price',
+                    style: Theme.of(
+                      context,
+                    ).textTheme.labelLarge?.copyWith(color: AppColors.primary),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
