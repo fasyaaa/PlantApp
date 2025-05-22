78ab22dc4a2e7c3b1a85490f2788bd2bae90302b
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-23 03:09:51 +0700

    ADD - maps state logic

--- lib/screens/home/bloc/gmaps_bloc/maps_state.dart
+++ lib/screens/home/bloc/gmaps_bloc/maps_state.dart
@@ -1,6 +1,38 @@
-part of 'maps_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-@immutable
-sealed class MapsState {}
+class MapState extends Equatable {
+  final LatLng? currentLocation;
+  final String? currentAddress;
+  final Marker? pickedMarker;
+  final String? pickedAddress;
+  final bool isLoading;
 
-final class MapsInitial extends MapsState {}
+  const MapState({
+    this.currentLocation,
+    this.currentAddress,
+    this.pickedMarker,
+    this.pickedAddress,
+    this.isLoading = true,
+  });
+
+  MapState copyWith({
+    LatLng? currentLocation,
+    String? currentAddress,
+    Marker? pickedMarker,
+    String? pickedAddress,
+    bool? isLoading,
+  }) {
+    return MapState(
+      currentLocation: currentLocation ?? this.currentLocation,
+      currentAddress: currentAddress ?? this.currentAddress,
+      pickedMarker: pickedMarker,
+      pickedAddress: pickedAddress,
+      isLoading: isLoading ?? this.isLoading,
+    );
+  }
+
+  @override
+  List<Object?> get props =>
+      [currentLocation, currentAddress, pickedMarker, pickedAddress, isLoading];
+}
