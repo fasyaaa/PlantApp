4f54451315765238e50bde34f3e9b0c1a2ad203a
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-22 07:45:19 +0700

    ADD - create HomeScreen with AppBar and menu icon

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dashboard"),
-        actions: const [],
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-            children: [],
-        ),
+    return Scaffold(appBar: buildAppBar());
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        onPressed: () {},
+        icon: SvgPicture.asset("assets/icons/menu.svg"),
       ),
     );
   }
