7a03bf5c9309e8ca6caaa83447986ad7c7c3c83d
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-22 11:52:12 +0700

    ADD - underline style for section title using Stack

--- lib/screens/home/components/title_with_more_bbtn.dart
+++ lib/screens/home/components/title_with_more_bbtn.dart
@@ -43,11 +43,28 @@ class TitleWithCUstomUnderline extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Dashboard"), actions: const []),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(children: []),
+    return Container(
+      height: 24,
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: AppSpacing.defaultPadding / 4),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.only(right: AppSpacing.defaultPadding / 4),
+              height: 7,
+              color: AppColors.primary.withValues(alpha: 0.2),
+            ),
+          ),
+        ],
       ),
     );
   }
