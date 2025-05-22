d54b943763297556b48a7b72485672b335cdbc03
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-22 10:54:42 +0700

    ADD - wrap with container

--- lib/screens/home/components/header_with_searchbox.dart
+++ lib/screens/home/components/header_with_searchbox.dart
@@ -10,74 +10,79 @@ class HeaderWithSearchBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: <Widget>[
-        Container(
-          height: size.height * 0.2,
-          decoration: BoxDecoration(
-            color: AppColors.primary,
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(36),
-              bottomRight: Radius.circular(36),
-            ),
-          ),
-          child: Row(
-            children: <Widget>[
-              Text(
-                'Hi Uishopy!',
-                style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Spacer(),
-              Image.asset('assets/images/logo.svg'),
-            ],
-          ),
-        ),
-        Positioned(
-          bottom: 0,
-          left: 0,
-          right: 0,
-          child: Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.symmetric(horizontal: AppSpacing.defaultPadding),
-            padding: EdgeInsets.symmetric(
-              horizontal: AppSpacing.defaultPadding,
-            ),
-            height: 54,
+    return Container(
+      margin: EdgeInsets.only(bottom: AppSpacing.defaultPadding * 2.5),
+      height: size.height * 0.2,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: AppSpacing.defaultPadding, right: AppSpacing.defaultPadding, bottom: 36 + AppSpacing.defaultPadding),
+            height: size.height * 0.2 - 27,
             decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(0, 10),
-                  blurRadius: 50,
-                  color: AppColors.primary.withValues(alpha: 0.23),
-                ),
-              ],
+              color: AppColors.primary,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
+              ),
             ),
             child: Row(
               children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    onChanged: (value) {},
-                    decoration: InputDecoration(
-                      hintText: "Search",
-                      hintStyle: TextStyle(
-                        color: AppColors.primary.withValues(alpha: 0.5),
-                      ),
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                    ),
+                Text(
+                  'Hi Uishopy!',
+                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                SvgPicture.asset("assets/icons/search.svg"),
+                Spacer(),
+                Image.asset('assets/images/logo.svg'),
               ],
             ),
           ),
-        ),
-      ],
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(horizontal: AppSpacing.defaultPadding),
+              padding: EdgeInsets.symmetric(
+                horizontal: AppSpacing.defaultPadding,
+              ),
+              height: 54,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: AppColors.primary.withValues(alpha: 0.23),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {},
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        hintStyle: TextStyle(
+                          color: AppColors.primary.withValues(alpha: 0.5),
+                        ),
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  SvgPicture.asset("assets/icons/search.svg"),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
