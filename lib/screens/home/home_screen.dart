27f3a86ed7af6242efbf2b4e45613b33ebc56fa9
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-23 01:42:05 +0700

    ADD - bottomNavigationBar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app/components/my_bottom_nav_bar.dart';
+import 'package:plant_app/screens/home/bloc/camera_bloc.dart';
 import 'package:plant_app/screens/home/components/body.dart';
 import 'package:plant_app/theme_constants.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: buildAppBar(), body: Body());
+    return Scaffold(
+        appBar: buildAppBar(),
+        body: Body(),
+        bottomNavigationBar: MyBottomNavBar(),
+    );
   }
 
   AppBar buildAppBar() {
