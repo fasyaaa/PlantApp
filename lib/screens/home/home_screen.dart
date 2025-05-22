2ddd916f769f556d0bc4a9570bc6b52b79acaea1
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-22 07:37:46 +0700

    ADD - Homescreen routh for main

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+            children: [],
+        ),
+      ),
+    );
+  }
+}
