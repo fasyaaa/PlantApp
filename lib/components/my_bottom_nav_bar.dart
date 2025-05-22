8aabd526b95c8d21d68efca72ffdb128a8c0e0ab
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-05-22 20:24:04 +0700

    ADD - MyBottomNavBar widget with flower, heart, and user icons

---
+++ lib/components/my_bottom_nav_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:plant_app/theme_constants.dart';
+
+class MyBottomNavBar extends StatelessWidget {
+  const MyBottomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        left: AppSpacing.defaultPadding * 2,
+        right: AppSpacing.defaultPadding * 2,
+        bottom: AppSpacing.defaultPadding,
+      ),
+      height: 80,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -10),
+            blurRadius: 35,
+            color: AppColors.primary.withValues(alpha: 0.38),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset("assets/icons/flower.svg"),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset("assets/icons/heart.svg"),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset("assets/icons/user.svg"),
+          ),
+        ],
+      ),
+    );
+  }
+}
